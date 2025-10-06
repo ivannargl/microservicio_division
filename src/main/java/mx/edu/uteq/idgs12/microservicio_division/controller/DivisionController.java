9fbcba2e69dedca73e20d9829d07f6cb285362b8
Alexis Lozada <alexislozadasalinas11@gmail.com>
2025-10-05 18:46:30 -0600

    agregar uso de dto para divisiones

--- src/main/java/mx/edu/uteq/idgs12/microservicio_division/controller/DivisionController.java
+++ src/main/java/mx/edu/uteq/idgs12/microservicio_division/controller/DivisionController.java
@@ -1,9 +1,8 @@
 package mx.edu.uteq.idgs12.microservicio_division.controller;
 
-import mx.edu.uteq.idgs12.microservicio_division.entity.Division;
+import mx.edu.uteq.idgs12.microservicio_division.dto.DivisionDTO;
 import mx.edu.uteq.idgs12.microservicio_division.service.DivisionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,10 +11,10 @@ public class DivisionController {
 
     @Autowired
     private DivisionService divisionService;
-    
+
     @PostMapping
-    public Division agregarDivision(@RequestBody Division division) {
-        return divisionService.agregarDivision(division);
+    public DivisionDTO agregarDivision(@RequestBody DivisionDTO divisionDTO) {
+        return divisionService.agregarDivision(divisionDTO);
     }
     
 }
--- src/main/java/mx/edu/uteq/idgs12/microservicio_division/service/DivisionService.java
+++ src/main/java/mx/edu/uteq/idgs12/microservicio_division/service/DivisionService.java
@@ -1,5 +1,6 @@
 package mx.edu.uteq.idgs12.microservicio_division.service;
 
+import mx.edu.uteq.idgs12.microservicio_division.dto.DivisionDTO;
 import mx.edu.uteq.idgs12.microservicio_division.entity.Division;
 import mx.edu.uteq.idgs12.microservicio_division.repository.DivisionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,20 @@ public class DivisionService {
     @Autowired
     private DivisionRepository divisionRepository;
     
-    public Division agregarDivision(Division division) {
+    public DivisionDTO agregarDivision(DivisionDTO divisionDTO) {
+        Division division = new Division();
+        division.setNombre(divisionDTO.getNombre());
         division.setActivo(true);
-        return divisionRepository.save(division);
+
+        Division guardada = divisionRepository.save(division);
+
+        // Convertir la entidad guardada en DTO de respuesta
+        DivisionDTO resultado = new DivisionDTO();
+        resultado.setId(guardada.getId());
+        resultado.setNombre(guardada.getNombre());
+        resultado.setActivo(guardada.isActivo());
+
+        return resultado;
     }
     
 }
