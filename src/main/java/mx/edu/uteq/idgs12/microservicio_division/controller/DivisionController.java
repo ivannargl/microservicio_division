860270d03d998e9e24369e14cbf87bee7e6e14bd
Alexis Lozada <alexislozadasalinas11@gmail.com>
2025-10-05 19:17:52 -0600

    agregar uso de dto para divisiones

--- src/main/java/mx/edu/uteq/idgs12/microservicio_division/controller/DivisionController.java
+++ src/main/java/mx/edu/uteq/idgs12/microservicio_division/controller/DivisionController.java
@@ -1,6 +1,6 @@
 package mx.edu.uteq.idgs12.microservicio_division.controller;
 
-import mx.edu.uteq.idgs12.microservicio_division.entity.Division;
+import mx.edu.uteq.idgs12.microservicio_division.dto.DivisionDTO;
 import mx.edu.uteq.idgs12.microservicio_division.service.DivisionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +14,8 @@ public class DivisionController {
     private DivisionService divisionService;
     
     @PutMapping("/{id}")
-    public ResponseEntity<Division> editarDivision(@PathVariable Long id, @RequestBody Division division) {
-        Division actualizada = divisionService.editarDivision(id, division);
+    public ResponseEntity<DivisionDTO> editarDivision(@PathVariable Long id, @RequestBody DivisionDTO divisionDTO) {
+        DivisionDTO actualizada = divisionService.editarDivision(id, divisionDTO);
         return ResponseEntity.ok(actualizada);
     }
     
--- src/main/java/mx/edu/uteq/idgs12/microservicio_division/service/DivisionService.java
+++ src/main/java/mx/edu/uteq/idgs12/microservicio_division/service/DivisionService.java
@@ -1,5 +1,6 @@
 package mx.edu.uteq.idgs12.microservicio_division.service;
 
+import mx.edu.uteq.idgs12.microservicio_division.dto.DivisionDTO;
 import mx.edu.uteq.idgs12.microservicio_division.entity.Division;
 import mx.edu.uteq.idgs12.microservicio_division.repository.DivisionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +12,21 @@ public class DivisionService {
     @Autowired
     private DivisionRepository divisionRepository;
     
-    public Division editarDivision(Long id, Division divisionDetalles) {
+    public DivisionDTO editarDivision(Long id, DivisionDTO divisionDTO) {
         Division divisionExistente = divisionRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("División no encontrada"));
 
-        divisionExistente.setNombre(divisionDetalles.getNombre());
-        divisionExistente.setActivo(divisionDetalles.isActivo());
+        divisionExistente.setNombre(divisionDTO.getNombre());
+        divisionExistente.setActivo(divisionDTO.isActivo());
 
-        return divisionRepository.save(divisionExistente);
+        Division actualizada = divisionRepository.save(divisionExistente);
+
+        DivisionDTO respuesta = new DivisionDTO();
+        respuesta.setId(actualizada.getId());
+        respuesta.setNombre(actualizada.getNombre());
+        respuesta.setActivo(actualizada.isActivo());
+
+        return respuesta;
     }
     
 }
